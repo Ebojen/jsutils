015ad0e34296cea4e3b994fda2781417711e5aed
Ebojen <32406068+Ebojen@users.noreply.github.com>
2022-09-25 10:14:18 -0500

    :construction: testing on main implementation

--- src/get_config.py
+++ src/get_config.py
@@ -8,30 +8,34 @@ import os
 from src.converter import ValueConverter
 from src.merger import merge_dicts
 
-if 'ENV' in os.environ:
-    default_environment = os.environ['ENV']
-else:
-    default_environment = 'test'
+
+def get_default_environment():
+    if 'ENV' in os.environ:
+        default_environment = os.environ['ENV']
+    else:
+        default_environment = 'test'
+
+    return default_environment
 
 
-def get_config(configuration_directory='./config',
-               environment=default_environment):
+def get_config(directory='./config',
+               environment=get_default_environment()):
     """Collects the default configuration and environment
     configuration from the configuration_directory and
     updates the default values with values from the
     environment configuration
     """
-    files = os.listdir(configuration_directory)
+    files = os.listdir(directory)
 
     if 'default.json' in files:
-        path = f'{configuration_directory}/default.json'
+        path = f'{directory}/default.json'
         with open(path, 'r', encoding='UTF-8') as default_file:
-            default_config = json.load(default_file.read())
+            default_config = json.load(default_file)
 
     if f'{environment}.json' in files:
-        path = f'{configuration_directory}/{environment}.json'
+        path = f'{directory}/{environment}.json'
         with open(path, 'r', encoding='UTF-8') as env_file:
-            env_config = json.load(env_file.read())
+            env_config = json.load(env_file)
     else:
         raise ImportError(
             f'No configuration for {environment} environment found.'
--- test/test_get_config.py
+++ test/test_get_config.py
@@ -1,5 +1,44 @@
-from src.get_config import get_config
+import json
+import importlib
+import os
+
+import pytest
+
+from src import get_config
 
 
 class TestGetConfig:
-    pass
+    def test_should_look_in_the_config_directory_by_default(self):
+        with pytest.raises(FileNotFoundError):
+            get_config.get_config()
+
+    def test_should_default_to_test_environment_if_not_set_or_provided(self, tmp_path):
+        d = tmp_path / 'config'
+        d.mkdir()
+        default = d / 'default.json'
+        default.write_text(json.dumps({'default_key': 'default_value'}))
+        test = d / 'test.json'
+        test.write_text(json.dumps({'test_key': 'test_value'}))
+
+        config = get_config.get_config(directory=d)
+
+        assert config.default_key == 'default_value'
+        assert config.test_key == 'test_value'
+
+    def test_should_use_the_ENV_value_for_default_if_available(self, tmp_path, monkeypatch):  # noqa: line-too-long
+        monkeypatch.setenv('ENV', 'test_env')
+        importlib.reload(get_config)
+        d = tmp_path / 'config'
+        d.mkdir()
+        default = d / 'default.json'
+        default.write_text(json.dumps({'default_key': 'default_value'}))
+        test = d / 'test.json'
+        test.write_text(json.dumps({'test_key': 'test_value'}))
+        test_env = d / 'test_env.json'
+        test_env.write_text(json.dumps({'test_env_key': 'test_env_value'}))
+
+        config = get_config.get_config(directory=d)
+
+        assert config.default_key == 'default_value'
+        assert config.test_env_key == 'test_env_value'
+        assert 'test_key' not in config._fields
