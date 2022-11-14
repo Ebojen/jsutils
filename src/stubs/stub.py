6de4bcb89dc64efb7d0b58547cb75842322c0116
Ebojen <32406068+Ebojen@users.noreply.github.com>
2022-11-14 16:38:23 -0600

    :construction: get_stub now stubs a method depending on args

--- src/stubs/stub.py
+++ src/stubs/stub.py
@@ -1,19 +1,25 @@
+from types import MethodType
 from typing import Callable
 
 
+"""
+TODO: From Sinon Stubs
+    * provided an object and method name, only stub the method and return the stub
+    * .createStubInstance
+    * .withArgs
+    * .value
+"""
+
+
 class Stub:
-    """
-    TODO: From Sinon Stubs
-      * .createStubInstance
-      * .withArgs
-      * .value
-    """
-    def __init__(self):
+    def __init__(self, original_object=None, method_name=None):
         self.call_count = 0
         self.call_history = []
         self.return_values = []
         self.fake = None
-        self.error = None
+        self.original_object = original_object
+        self.method_name = method_name
+        self.original_behavior = getattr(self.original_object, method_name) if method_name else None
 
     def returns(self, return_value: any) -> None:
         self.return_values.append(return_value)
@@ -45,6 +51,17 @@ class Stub:
     def raises(self, error):
         return self.returns(error)
 
+    def restore(self):
+        if self.original_object and self.original_behavior:
+            setattr(
+                self.original_object,
+                self.method_name,
+                self.original_behavior,
+            )
+        self.call_count = 0
+        self.call_history = []
+        self.fake = None
+
     def __call__(self, *args, **kwargs) -> any:
         self.call_count += 1
         self.call_history.append((args, kwargs))
@@ -58,3 +75,12 @@ class Stub:
                 raise return_value
             return self.return_values[self.call_count - 1]
         return None
+
+
+def get_stub(obj: any = None, method: str = None) -> Stub:
+    if obj is not None and method is not None:
+        method_stub = Stub(original_object=obj, method_name=method)
+        setattr(obj, method, method_stub)
+        return method_stub
+
+    return Stub()
--- test/stubs/test_stub.py
+++ test/stubs/test_stub.py
@@ -1,10 +1,11 @@
 import pytest
 
-from src.stubs.stub import Stub
+from src.stubs.stub import Stub, get_stub
+
 
 class TestStub:
     def test_should_track_the_number_of_calls_to_the_stubs(self):
-        stub = Stub()
+        stub = get_stub()
         assert stub.call_count == 0
 
         stub()
@@ -17,80 +18,94 @@ class TestStub:
         assert stub.call_count == 3
 
     def test_should_track_the_call_history(self):
-        stub = Stub()
+        stub = get_stub()
         expected1 = (
-            ('arg1', 'arg2'),
-            {'kwarg_key1': 'kwarg1', 'kwarg_key2': 'kwarg2'}
+            ("arg1", "arg2"),
+            {"kwarg_key1": "kwarg1", "kwarg_key2": "kwarg2"},
         )
-        stub('arg1', 'arg2', kwarg_key1='kwarg1', kwarg_key2='kwarg2')
+        stub(
+            "arg1", "arg2",
+            kwarg_key1="kwarg1",
+            kwarg_key2="kwarg2",
+        )
 
         assert stub.call_history == [expected1]
 
         expected2 = (
-            ('arg3',),
-            {'kwarg_key3': 'kwarg3', 'kwarg_key4': 'kwarg4', 'kwarg_key5': 'kwarg5'}
+            ("arg3",),
+            {
+                "kwarg_key3": "kwarg3",
+                "kwarg_key4": "kwarg4",
+                "kwarg_key5": "kwarg5",
+            },
         )
-        stub('arg3', kwarg_key3='kwarg3', kwarg_key4='kwarg4', kwarg_key5='kwarg5')
+        stub(
+            "arg3",
+            kwarg_key3="kwarg3",
+            kwarg_key4="kwarg4",
+            kwarg_key5="kwarg5"
+        )
 
         assert stub.call_history == [expected1, expected2]
 
     def test_should_return_the_provided_return_value(self):
-        stub = Stub()
-        stub.returns('test_value')
+        stub = get_stub()
+        stub.returns("test_value")
 
         result = stub()
 
-        assert result == 'test_value'
+        assert result == "test_value"
 
     def test_should_return_the_configured_return_values(self):
-        stub = Stub()
-        stub.on_call_count(0).returns('test_value_1') \
-            .on_call_count(1).returns(8675309)
+        stub = get_stub()
+        stub.on_call_count(0).returns("test_value_1").on_call_count(1).returns(8675309)  # noqa: E501
 
         result_1 = stub()
         result_2 = stub()
 
-        assert result_1 == 'test_value_1'
+        assert result_1 == "test_value_1"
         assert result_2 == 8675309
 
     def test_should_configure_the_first_call(self):
-        stub = Stub()
-        stub.on_first_call().returns('first_call')
+        stub = get_stub()
+        stub.on_first_call().returns("first_call")
 
         result = stub()
 
-        assert result == 'first_call'
+        assert result == "first_call"
 
     def test_should_configure_the_second_call(self):
-        stub = Stub()
-        stub.on_first_call().returns('first_call') \
-            .on_second_call().returns('second_call')
+        stub = get_stub()
+        stub.on_first_call().returns("first_call").on_second_call().returns(
+            "second_call"
+        )
 
         result_1 = stub()
         result_2 = stub()
 
-        assert result_1 == 'first_call'
-        assert result_2 == 'second_call'
+        assert result_1 == "first_call"
+        assert result_2 == "second_call"
 
     def test_should_configure_the_third_call(self):
-        stub = Stub()
-        stub.on_first_call().returns('first_call') \
-            .on_second_call().returns('second_call') \
-            .on_third_call().raises(ValueError('Its too much'))
+        stub = get_stub()
+        stub.on_first_call().returns("first_call").on_second_call().returns(
+            "second_call"
+        ).on_third_call().raises(ValueError("Its too much"))
 
         result_1 = stub()
         result_2 = stub()
 
-        assert result_1 == 'first_call'
-        assert result_2 == 'second_call'
+        assert result_1 == "first_call"
+        assert result_2 == "second_call"
 
-        with pytest.raises(ValueError, match='Its too much'):
+        with pytest.raises(ValueError, match="Its too much"):
             stub()
 
     def test_should_call_the_provided_fake(self):
         def add_two(num):
             return num + 2
-        stub = Stub()
+
+        stub = get_stub()
         stub.calls_fake(add_two)
 
         result_1 = stub(50)
@@ -100,16 +115,53 @@ class TestStub:
         assert result_2 == -6999
 
         def concat_str(first_str=None, last_str=None):
-            return f'{first_str}{last_str}'
-        stub2 = Stub()
-        stub2.calls_fake(concat_str)
-        result_3 = stub2(first_str='StringOne', last_str='StringTwo')
+            return f"{first_str}{last_str}"
 
-        assert result_3 == 'StringOneStringTwo'
+        stub2 = get_stub()
+        stub2.calls_fake(concat_str)
+        result_3 = stub2(first_str="StringOne", last_str="StringTwo")
+
+        assert result_3 == "StringOneStringTwo"
 
     def test_should_throw_the_provided_error(self):
-        stub = Stub()
-        stub.raises(RuntimeError('Boom!'))
+        stub = get_stub()
+        stub.raises(RuntimeError("Boom!"))
 
-        with pytest.raises(RuntimeError, match='Boom!'):
+        with pytest.raises(RuntimeError, match="Boom!"):
             stub()
+
+    def test_should_replace_the_given_method_on_an_obj(self):
+        class TestClass:
+            def add_three(num):
+                return num + 3
+
+        obj = TestClass()
+        stub = get_stub(obj, "add_three")
+
+        assert isinstance(stub, Stub)
+
+    def test_should_allow_configuration_of_stubbed_method(self):
+        class TestClass:
+            def add_three(self, num):
+                return num + 3
+
+        obj = TestClass()
+        stub = get_stub(obj, "add_three")
+        stub.returns(42)
+
+        result = obj.add_three(5)
+
+        assert result == 42
+
+    def test_should_restore_original_behavior_to_stubbed_method(self):
+        class TestClass:
+            def add_three(self, num):
+                return num + 3
+
+        obj = TestClass()
+        get_stub(obj, "add_three")
+        obj.add_three.restore()
+
+        result = obj.add_three(5)
+
+        assert result == 8
